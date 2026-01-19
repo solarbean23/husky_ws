2800a3c74df3fbf38cc67d379b59a95b90767650
solarbean23 <lsbean991@gmail.com>
2026-01-19 18:24:05 +0900

    Update launch files and setup to support scalable robot deployment

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -1,59 +1,53 @@
-# import os
-# import launch
-# from launch import LaunchDescription
-# from ament_index_python.packages import get_package_share_directory
-# from webots_ros2_driver.webots_launcher import WebotsLauncher
-# from webots_ros2_driver.webots_controller import WebotsController
-# def generate_launch_description():
-#     package_dir = get_package_share_directory('webots_ros2_husky')
-    
-#     # Start a Webots simulation instance
-#     webots = WebotsLauncher(
-#         world=os.path.join(package_dir, 'worlds', 'husky_world.wbt')
-#     )
-
-#     # ROS control spawners
-#     ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yaml')
-    
-#     # Create a ROS node interacting with the simulated robot
-#     robot_description_path = os.path.join(package_dir, 'resource', 'husky.urdf')
-#     robot_driver = WebotsController(
-#         robot_name='Husky',
-#         parameters=[
-#             {'robot_description': robot_description_path},
-#             ros2_control_params
-#         ]
-#     )
-
-#     return LaunchDescription([
-#         webots,
-#         robot_driver,
-
-#         # The following action will kill all nodes once the Webots simulation has exited
-#         launch.actions.RegisterEventHandler(
-#             event_handler=launch.event_handlers.OnProcessExit(
-#                 target_action=webots,
-#                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-#         )
-#     )
-# ])
-
 import os
+import re
 import launch
 from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable  # ✅ 추가
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
 from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
 from launch_ros.actions import Node
 
+
+def discover_robots_from_world(world_path):
+    """월드 파일에서 Husky 기반 로봇들의 DEF 이름을 추출"""
+    robot_names = []
+    # DEF <name> Husky, DEF <name> Fire_UGV, DEF <name> Rescue_UGV 등 매칭
+    pattern = re.compile(r'^DEF\s+(\w+)\s+(Husky|Fire_UGV|Rescue_UGV)\b', re.MULTILINE)
+    
+    with open(world_path, 'r') as f:
+        content = f.read()
+    
+    for match in pattern.finditer(content):
+        robot_names.append(match.group(1))
+    
+    return robot_names
+
+
 def generate_launch_description():
     package_dir = get_package_share_directory('webots_ros2_husky')
 
+    # ✅ 기존 값이 있으면 보존하면서 package_dir을 추가
+    prev_extra = os.environ.get('WEBOTS_EXTRA_PROJECT_PATH', '')
+    extra_path = package_dir if prev_extra == '' else (package_dir + os.pathsep + prev_extra)
+
+    prev_proj = os.environ.get('WEBOTS_PROJECT_PATH', '')
+    proj_path = package_dir if prev_proj == '' else (package_dir + os.pathsep + prev_proj)
+
+    # ✅ Webots가 /tmp world를 열더라도 여기 경로에서 controllers를 찾게 됨
+    set_webots_paths = [
+        SetEnvironmentVariable('WEBOTS_EXTRA_PROJECT_PATH', extra_path),
+        SetEnvironmentVariable('WEBOTS_PROJECT_PATH', proj_path),
+    ]
+
     # Start a Webots simulation instance
-    webots = WebotsLauncher(
-        world=os.path.join(package_dir, 'worlds', 'fire_rescue_world.wbt')
-    )
+    world_path = os.path.join(package_dir, 'worlds', 'fire_rescue_world.wbt')
+    webots = WebotsLauncher(world=world_path)
+
+    # 월드 파일에서 로봇 이름 동적 발견
+    robot_names = discover_robots_from_world(world_path)
+    print(f"[robot_launch] Discovered robots: {robot_names}")
 
     # Create the robot state publisher
     robot_state_publisher = Node(
@@ -68,6 +62,7 @@ def generate_launch_description():
     # ROS control spawners
     ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yaml')
     controller_manager_timeout = ['--controller-manager-timeout', '50']
+
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
@@ -81,41 +76,63 @@ def generate_launch_description():
         arguments=['diffdrive_controller'] + controller_manager_timeout,
     )
     ros_control_spawners = [joint_state_broadcaster_spawner, diffdrive_controller_spawner]
-    
-    mappings = [
-        ('/diffdrive_controller/cmd_vel', '/cmd_vel'), 
-        ('/diffdrive_controller/odom', '/odom'),
-    ]    
 
-    # Create a ROS node interacting with the simulated robot
     robot_description_path = os.path.join(package_dir, 'resource', 'husky.urdf')
-    robot_driver = WebotsController(
-        robot_name='Husky',
-        parameters=[
-            {'robot_description': robot_description_path,
-             'set_robot_state_publisher': True
-             },
-            ros2_control_params
-        ],
-        remappings=mappings
-    )
 
-    # Wait for the simulation to be ready to start navigation nodes
-    waiting_nodes = WaitForControllerConnection(
-        target_driver=robot_driver,
-        nodes_to_start=ros_control_spawners
-    )    
+    # ----------------------------------------
+    # 로봇별 동적 생성: mappings, robot_driver, waiting_nodes
+    # ----------------------------------------
+    robot_drivers = []
+    waiting_nodes = []
 
-    return LaunchDescription([
+    for i, robot_name in enumerate(robot_names):
+        # 각 로봇별 remapping
+        mappings = [
+            ('/diffdrive_controller/cmd_vel', f'/{robot_name}/cmd_vel'),
+            ('/diffdrive_controller/odom', f'/{robot_name}/odom'),
+        ]
+
+        # 각 로봇별 WebotsController
+        robot_driver = WebotsController(
+            robot_name=robot_name,
+            parameters=[
+                {'robot_description': robot_description_path,
+                 'set_robot_state_publisher': True},
+                ros2_control_params
+            ],
+            remappings=mappings
+        )
+        robot_drivers.append(robot_driver)
+
+        # 마지막 로봇은 waiting_nodes 제외
+        if i < len(robot_names) - 1:
+            waiting_node = WaitForControllerConnection(
+                target_driver=robot_driver,
+                nodes_to_start=ros_control_spawners
+            )
+            waiting_nodes.append(waiting_node)
+
+    # LaunchDescription 구성: robot_driver와 waiting_nodes 교차 배치
+    launch_items = [
+        *set_webots_paths,   # ✅ webots 앞에 들어가야 함
         webots,
         robot_state_publisher,
-        robot_driver,
-        waiting_nodes,
-        # The following action will kill all nodes once the Webots simulation has exited
+    ]
+
+    # robot_drivers와 waiting_nodes 교차 배치 (마지막 로봇은 waiting_node 없음)
+    for i, robot_driver in enumerate(robot_drivers):
+        launch_items.append(robot_driver)
+        if i < len(waiting_nodes):
+            launch_items.append(waiting_nodes[i])
+
+    launch_items.append(
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
             )
         )
-    ])
+    )
+
+    return LaunchDescription(launch_items)
+    
--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ package_name = 'webots_ros2_husky'
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name + '/protos', ['protos/Husky.proto']))
+data_files.append(('share/' + package_name + '/protos', ['protos/Fire_UGV.proto']))
+data_files.append(('share/' + package_name + '/protos', ['protos/Rescue_UGV.proto']))
 data_files.append(('share/' + package_name + '/worlds', ['worlds/husky_world.wbt']))
 data_files.append(('share/' + package_name + '/worlds', ['worlds/fire_rescue_world.wbt']))
 data_files.append(('share/' + package_name + '/resource', ['resource/ros2control.yaml']))
 data_files.append(('share/' + package_name + '/resource', ['resource/husky.urdf']))
 data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
 data_files.append(('share/' + package_name, ['package.xml']))
-data_files.append(('share/' + package_name + '/protos/icons', ['protos/icons/base.jpg']))
+data_files.append(('share/' + package_name + '/worlds/icons', ['worlds/icons/base.png']))
+data_files.append(('share/' + package_name + '/worlds/icons', ['worlds/icons/rescue_white.png']))
+data_files.append(('share/' + package_name + '/worlds/icons', ['worlds/icons/rescue_red.png']))
+data_files.append(('share/' + package_name + '/controllers/world_supervisor', ['controllers/world_supervisor/world_supervisor.py']))
+data_files.append(('share/' + package_name + '/controllers/robot_supervisor', ['controllers/robot_supervisor/robot_supervisor.py']))
 
 setup(
     name=package_name,
